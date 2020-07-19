824f7055be155da17de7338bcd44f17977a5f6c5
Prahveen Thiruchelvam <prahveent@gmail.com>
2020-07-19 22:28:46 +0530

    initial commit

---
+++ FeatureToggle/Controllers/FeatureToggleController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+
+namespace FeatureToggle.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class FeatureToggleController : ControllerBase
+	{
+		private readonly IFeatureManager _featureManager;
+
+		public FeatureToggleController(IFeatureManager featureManager)
+		{
+			_featureManager = featureManager;
+		}
+
+		[HttpGet("price")]
+		public async Task<IActionResult> GetPrice()
+		{
+			decimal price = 100;
+			if (await _featureManager.IsEnabledAsync(Features.GetPrice))
+			{
+				// half done feature
+				price = GetPriceBasedOnMetrix();
+			}
+			return this.Ok(price);
+		}
+
+		[HttpGet("count")]
+		public async Task<IActionResult> GetCount()
+		{
+			int count = 10;
+			if (await _featureManager.IsEnabledAsync(Features.GetCount))
+			{
+				count = GetCountWithSomeLogic();
+			}
+			return this.Ok(count);
+		}
+
+
+		private decimal GetPriceBasedOnMetrix()
+		{
+			return (decimal) 500.00;
+		}
+
+		private int GetCountWithSomeLogic()
+		{
+			return 40 * 10;
+		}
+
+	}
+}
+
---
+++ FeatureToggle/Controllers/Features.cs
@@ -0,0 +1,8 @@
+﻿namespace FeatureToggle.Controllers
+{
+	public static class Features
+	{
+		public const string GetPrice = "GetPrice";
+		public const string GetCount = "GetCount";
+	}
+}
